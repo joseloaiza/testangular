6a74bab496c5cc6c908db1d92b9ce9f2b3e002f7
joseloaiza <joseloaiza815@gmail.com>
2023-06-24 00:24:47 -0400

    update terraform files

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,28 +1,28 @@
 //>>Uncomment this section once EKS is created - Start
-#  data "aws_eks_cluster" "cluster" {
-#    name = "eksclusterdemo" #module.in28minutes-cluster.cluster_name
-#  }
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = "ieksclusterdemo" #module.in28minutes-cluster.cluster_name
-# }
-# # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# # ServiceAccount needs permissions to create deployments 
-# # and services in default namespace
-# resource "kubernetes_cluster_role_binding" "example" {
-#   metadata {
-#     name = "fabric8-rbac"
-#   }
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = "default"
-#     namespace = "default"
-#   }
-# }
+  data "aws_eks_cluster" "cluster" {
+    name = "eksclusterdemo" #module.in28minutes-cluster.cluster_name
+  }
+ data "aws_eks_cluster_auth" "cluster" {
+   name = "eksclusterdemo" #module.in28minutes-cluster.cluster_name
+ }
+ # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+ # ServiceAccount needs permissions to create deployments 
+ # and services in default namespace
+ resource "kubernetes_cluster_role_binding" "example" {
+   metadata {
+     name = "fabric8-rbac"
+   }
+   role_ref {
+     api_group = "rbac.authorization.k8s.io"
+     kind      = "ClusterRole"
+     name      = "cluster-admin"
+   }
+   subject {
+     kind      = "ServiceAccount"
+     name      = "default"
+     namespace = "default"
+   }
+ }
 //>>Uncomment this section once EKS is created - End
 
 provider "aws" {
