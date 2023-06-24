99afc22241e2e4101c9822df8bb0f6fd0f7a1b4a
joseloaiza <joseloaiza815@gmail.com>
2023-06-24 00:05:12 -0400

    update terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,41 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  //>>Uncomment this section once EKS is created - Start
+   host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
+   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+   token                  = data.aws_eks_cluster_auth.cluster.token
+  //>>Uncomment this section once EKS is created - End
+}
+
+
+//>>Uncomment this section once EKS is created - Start
+  data "aws_eks_cluster" "cluster" {
+    name = "eksclusterdemo" #module.in28minutes-cluster.cluster_name
+  }
+ data "aws_eks_cluster_auth" "cluster" {
+   name = "ieksclusterdemo" #module.in28minutes-cluster.cluster_name
+ }
+ # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+ # ServiceAccount needs permissions to create deployments 
+ # and services in default namespace
+ resource "kubernetes_cluster_role_binding" "example" {
+   metadata {
+     name = "fabric8-rbac"
+   }
+   role_ref {
+     api_group = "rbac.authorization.k8s.io"
+     kind      = "ClusterRole"
+     name      = "cluster-admin"
+   }
+   subject {
+     kind      = "ServiceAccount"
+     name      = "default"
+     namespace = "default"
+   }
+ }
+//>>Uncomment this section once EKS is created - End
 
 #-----------------------
 # EKS CLuster Definition
