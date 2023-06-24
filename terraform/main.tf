a70e511b590b6786aff99b3136daa844bc9fbfbe
joseloaiza <joseloaiza815@gmail.com>
2023-06-23 20:55:48 -0400

    update terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-state-jlq" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+
 #-----------------------
 # EKS CLuster Definition
 #-----------------------
