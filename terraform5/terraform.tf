a57b288821c7deda180c179a503e646b06172918
joseloaiza <joseloaiza815@gmail.com>
2023-06-28 18:26:45 -0400

    test created task and service

--- terraform5/terraform.tf
+++ terraform5/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
-  provider "aws" {
-    region  = "us-east-1"
-  }
-
    backend "s3" {
     bucket = "terraform-backend-state-jlq" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
     region = "us-east-1"
   }
 }
+
+  provider "aws" {
+    region  = "us-east-1"
+  }
